bb699c3093c96dce42786f0a3abc61e0269700db
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-22 22:00:37 -0400

    scraper complete

--- scrape.py
+++ scrape.py
@@ -1,3 +1,22 @@
+from bs4 import BeautifulSoup
 import requests
-URL = 'https://www.rockethomes.com/fl/orlando'
-page = requests.get(URL)
+from csv import writer
+
+url= "https://www.century21.com/real-estate/orlando-fl/LCFLORLANDO/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+lists = soup.find_all('div', class_="property-card-primary-info")
+
+with open('housing.csv', 'w', encoding='utf8', newline='') as f:
+    thewriter = writer(f)
+    header = ['Title', 'Location', 'Price', 'Area']
+    thewriter.writerow(header)
+
+    for list in lists:
+
+        price = list.find('a', class_="listing-price").text.replace('\n', '')
+        beds = list.find('div', class_="property-beds").text.replace('\n', '')
+        
+        info = [price, beds]
+        thewriter.writerow(info)
