c119464094da699871fd30da42fb230db523cbdc
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-22 22:13:02 -0400

    scraper additions

--- scrape.py
+++ scrape.py
@@ -10,13 +10,14 @@ lists = soup.find_all('div', class_="property-card-primary-info")
 
 with open('housing.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    header = ['Title', 'Location', 'Price', 'Area']
+    header = ['Price', 'Beds', 'Baths', 'Sqft']
     thewriter.writerow(header)
 
     for list in lists:
-
         price = list.find('a', class_="listing-price").text.replace('\n', '')
         beds = list.find('div', class_="property-beds").text.replace('\n', '')
+        baths = list.find('div', class_="property-baths").text.replace('\n', '')
+        sqft = list.find('div', class_="property-sqft").text.replace('\n', '')
         
-        info = [price, beds]
+        info = [price, beds, baths, sqft]
         thewriter.writerow(info)
