777877b005368bc691578f22032f86d69d7c0286
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-28 20:31:50 -0400

    fixes

--- scrape.py
+++ scrape.py
@@ -3,21 +3,22 @@ import requests
 from requests_html import HTML, HTMLSession
 from csv import writer
 from urllib.request import urlopen
-url= "https://www.realtor.com/realestateandhomes-search/Orlando_FL"
+
+url= "https://www.century21.com/real-estate/orlando-fl/LCFLORLANDO/"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
+lists = soup.find_all('div', class_="property-card-primary-info")
 
-with open('listing.csv', 'w', encoding='utf8', newline='') as f:
+with open('housing.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     header = ['Price', 'Beds', 'Baths']
     thewriter.writerow(header)
 
     for list in lists:
-        price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
-        beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
-        baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
+        price = list.find('div', attrs={'class': 'listing-price'})
+        beds = list.find('div', attrs={'class': 'property-beds'}).text.replace('\n', '')
+        baths = list.find('div', attrs={'class': 'property-baths'}).text.replace('\n', '')
         
         info = [price, beds, baths]
         thewriter.writerow(info)
