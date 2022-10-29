42b76105d70c8c41d75095a36de08f92f937adb3
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-28 21:14:07 -0400

    data clearning function complete

--- scraper2.0.py
+++ scraper2.0.py
@@ -19,13 +19,16 @@ lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
 
 with open('listing.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    header = ['Price', 'Beds', 'Baths']
+    header = ['Price', 'Beds', 'Baths','Sqft']
     thewriter.writerow(header)
 
     for list in lists:
         price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
         beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
         baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
-        
-        info = [price, beds, baths]
+        sqft = list.find('li', attrs={'data-label': 'pc-meta-sqft'}).text.replace('\n', '')
+
+        #info["Price"] = pd.to_numeric(info["price"], errors='coerce').fillna(0, downcast='infer')
+
+        info = [price, beds, baths,sqft]
         thewriter.writerow(info)
