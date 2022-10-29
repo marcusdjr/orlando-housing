2a0347ef1f2bb670a9fd95ab0f14890506e878de
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-29 14:14:13 -0400

    ploomber pipline complete

--- housing/scripts/clean.py
+++ housing/scripts/clean.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
@@ -21,11 +28,64 @@
 # %% tags=["parameters"]
 # If this task has dependencies, list them them here
 # (e.g. upstream = ['some_task']), otherwise leave as None.
-upstream = None
+upstream = ['get']
 
 # This is a placeholder, leave it as None
 product = None
 
 
 # %%
-# your code here...
+#import some necessary librairies
+
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x)) #Limiting floats output to 3 decimal points
+
+df = pd.read_csv('listing.csv')
+
+#Converting Price value into float datatype
+def clean_currency(x):
+    """ If the value is a string, then remove currency symbol and delimiters
+    otherwise, the value is numeric and can be converted
+    """
+    if isinstance(x, str):
+        return(x.replace('$', '').replace(',', ''))
+    return(x)
+
+df['Price'] = df['Price'].apply(clean_currency).astype('float')
+
+#Converting Beds value into float datatype
+def clean_currency(x):
+    """ If the value is a string, then remove currency symbol and delimiters
+    otherwise, the value is numeric and can be converted
+    """
+    if isinstance(x, str):
+        return(x.replace('bed', '').replace(',', ''))
+    return(x)
+
+df['Beds'] = df['Beds'].apply(clean_currency).astype('float')
+
+#Converting Baths value into float datatype
+def clean_currency(x):
+    """ If the value is a string, then remove currency symbol and delimiters
+    otherwise, the value is numeric and can be converted
+    """
+    if isinstance(x, str):
+        return(x.replace('bath', '').replace(',', ''))
+    return(x)
+
+df['Baths'] = df['Baths'].apply(clean_currency).astype('float')
+
+#Converting Sqft value into float datatype
+def clean_currency(x):
+    """ If the value is a string, then remove currency symbol and delimiters
+    otherwise, the value is numeric and can be converted
+    """
+    if isinstance(x, str):
+        return(x.replace('sqft', '').replace(',', ''))
+    return(x)
+
+df['Sqft'] = df['Sqft'].apply(clean_currency).astype('float')
+
+# %%
--- housing/scripts/get.py
+++ housing/scripts/get.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
@@ -28,4 +35,39 @@ product = None
 
 
 # %%
-# your code here...
+from matplotlib.dviread import Page
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from csv import writer
+
+url = "https://api.webscrapingapi.com/v1"
+params = {
+ "api_key": "WN5PAncn1XB2mzveYazxnJpsoIAq8KB8",
+ "url": "https://www.realtor.com/realestateandhomes-search/Orlando_FL"
+}
+
+response = requests.request("GET", url, params=params)
+
+content = response.text
+
+soup = BeautifulSoup(response.content, 'html.parser')
+lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
+
+with open('listing.csv', 'w', encoding='utf8', newline='') as f:
+    thewriter = writer(f)
+    header = ['Price', 'Beds', 'Baths','Sqft']
+    thewriter.writerow(header)
+
+    for list in lists:
+        price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
+        beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
+        baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
+        sqft = list.find('li', attrs={'data-label': 'pc-meta-sqft'}).text.replace('\n', '')
+
+        #info["Price"] = pd.to_numeric(info["price"], errors='coerce').fillna(0, downcast='infer')
+
+        info = [price, beds, baths,sqft]
+        thewriter.writerow(info)
+
+# %%
