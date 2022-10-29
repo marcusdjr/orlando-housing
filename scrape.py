675c6278f55ed647aff6432d54b0a6513d992649
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-29 13:45:53 -0400

    addition of ploomber and configuration of pipline

--- scrape.py
+++ scrape.py
@@ -4,21 +4,25 @@ from requests_html import HTML, HTMLSession
 from csv import writer
 from urllib.request import urlopen
 
-url= "https://www.century21.com/real-estate/orlando-fl/LCFLORLANDO/"
+
+url= "https://www.realtor.com/realestateandhomes-search/Orlando_FL"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-lists = soup.find_all('div', class_="property-card-primary-info")
+lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
 
 with open('housing.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    header = ['Price', 'Beds', 'Baths']
+    header = ['Price', 'Beds', 'Baths','Sqft']
     thewriter.writerow(header)
 
     for list in lists:
-        price = list.find('div', attrs={'class': 'listing-price'})
-        beds = list.find('div', attrs={'class': 'property-beds'}).text.replace('\n', '')
-        baths = list.find('div', attrs={'class': 'property-baths'}).text.replace('\n', '')
-        
-        info = [price, beds, baths]
+        price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
+        beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
+        baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
+        sqft = list.find('li', attrs={'data-label': 'pc-meta-sqft'}).text.replace('\n', '')
+
+        #info["Price"] = pd.to_numeric(info["price"], errors='coerce').fillna(0, downcast='infer')
+
+        info = [price, beds, baths,sqft]
         thewriter.writerow(info)
