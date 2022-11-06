0db4c846fe0d7d2ce81024a4a877d8e3431fff67
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-11-06 18:08:23 -0500

    establishing scraper

--- pipeline/scripts/get.py
+++ pipeline/scripts/get.py
@@ -40,33 +40,46 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from csv import writer
+import creds
+import requests
 
-url = "https://api.webscrapingapi.com/v1"
-params = {
- "api_key": "WN5PAncn1XB2mzveYazxnJpsoIAq8KB8",
- "url": "https://www.realtor.com/realestateandhomes-search/Orlando_FL"
-}
+url = "https://us-real-estate.p.rapidapi.com/sold-homes"
 
-response = requests.request("GET", url, params=params)
-content = response.text
-soup = BeautifulSoup(response.content, 'html.parser')
+querystring = {"state_code":"FL","city":"Orlando","limit":"200","offset":"0","sort":"sold_date"}
 
-lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
-with open('housing.csv', 'w', encoding='utf8', newline='') as f:
-    thewriter = writer(f)
-    header = ['Price', 'Beds', 'Baths','Sqft']
-    thewriter.writerow(header)
+response = requests.request("GET", url, headers=creds.headers, params=querystring)
 
-    for list in lists:
-        price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
-        beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
-        baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
-        sqft = list.find('li', attrs={'data-label': 'pc-meta-sqft'}).text.replace('\n', '')
+with open("sold.csv","w") as file:
+    file.write(response.text + "\n")
 
-        info = [price, beds, baths,sqft]
-        thewriter.writerow(info)
+# df = pd.DataFrame(querystring{"state_code":"FL","city":"Orlando","limit":"200","offset":"0","sort":"sold_date"})
+# df.to_csv('soldhomes.csv',index=False)
+
+# df = pd.read_csv('soldhomes.csv')
+# df.to_csv(product['data'], index=False)
+
+# url = "https://api.webscrapingapi.com/v1"
+
+# response = requests.request("GET", url, params=creds.params)
+# content = response.text
+# soup = BeautifulSoup(response.content, 'html.parser')
+
+# lists = soup.find_all('div', class_="jsx-11645185 summary-wrap")
+# with open('housing.csv', 'w', encoding='utf8', newline='') as f:
+#     thewriter = writer(f)
+#     header = ['Price', 'Beds', 'Baths','Sqft']
+#     thewriter.writerow(header)
+
+#     for list in lists:
+#         price = list.find('span', attrs={'data-label': 'pc-price'}).text.replace('\n', '')
+#         beds = list.find('li', attrs={'data-label': 'pc-meta-beds'}).text.replace('\n', '')
+#         baths = list.find('li', attrs={'data-label': 'pc-meta-baths'}).text.replace('\n', '')
+#         sqft = list.find('li', attrs={'data-label': 'pc-meta-sqft'}).text.replace('\n', '')
+
+#         info = [price, beds, baths,sqft]
+#         thewriter.writerow(info)
         
-df = pd.read_csv('housing.csv')
-df.to_csv(product['data'], index=False)
+# df = pd.read_csv('housing.csv')
+# df.to_csv(product['data'], index=False)
 
 # %%
